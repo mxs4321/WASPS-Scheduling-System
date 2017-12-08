a0e39925cd531c3214792ba5ebfbb8553b1da0a8
Brett Lamy <bel423@me.com>
2017-12-08 09:33:43 -0500

    added styled-components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './controller/App';
+import registerServiceWorker from './util/registerServiceWorker';
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  html, body, #root {
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;    
+    padding: 0;
+    margin: 0;
+    background-color: #F2F2F2;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    overflow: hidden;
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
