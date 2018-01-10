f542b9bc4a594fdfcd4a6d54ac5c55f14e7dad29
Brett Lamy <bel423@me.com>
2018-01-10 11:39:07 -0500

    added role to database

--- database/users.sql
+++ database/users.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
+  `role` ENUM('Passanger', 'dispatcher', 'driver') NOT NULL,
   `phoneNumber` int(11) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
   `city` varchar(255) NOT NULL,
@@ -16,5 +17,5 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `firstName`, `lastName`, `phoneNumber`, `address`, `city`, `zip`, `email`, `password`, `salt`) VALUES
-(1, 'Brett', 'Lamy', 2035254835, '59 Werner Park', 'Rochester, Ny', 14620, 'bel9708@g.rit.edu', '$2y$10$TDAwo5v.lcakEOL.ibMRK.G5oTGBPYEmQtMTvm/Bvn1pHsgUfu3r.', '4&3vF');
+INSERT INTO `users` (`id`, `firstName`, `role`, `lastName`, `phoneNumber`, `address`, `city`, `zip`, `email`, `password`, `salt`) VALUES
+(1, 'Brett', 'Lamy', 'dispatcher', 2035254835, '59 Werner Park', 'Rochester, Ny', 14620, 'bel9708@g.rit.edu', '$2y$10$TDAwo5v.lcakEOL.ibMRK.G5oTGBPYEmQtMTvm/Bvn1pHsgUfu3r.', '4&3vF');
