38cd35c598e379ef08ccae61f4f5b133503e4237
Unknown <bel423@me.com>
2018-01-21 12:56:37 -0500

    Added super test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,3 +1,22 @@
-it('renders without crashing', () => {
-  expect(1).toEqual(1);
+import Request from 'supertest';
+
+const request = Request('http://localhost:8000');
+
+describe('GET /', () => {
+  it('responds with html', async () => {
+    await request
+      .get('/')
+      .expect('Content-Type', /html/)
+      .expect(200);
+  });
+});
+
+describe('GET /api/users.php', () => {
+  it('respond with json', async () => {
+    await request
+      .get('/api/users.php')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+  });
 });
