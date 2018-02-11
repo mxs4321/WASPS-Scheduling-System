6e69d7dcfe1ba4dfb564f30130a40d6aeb08c44b
Lucano Zganec <lxz7884@g.rit.edu>
2018-02-11 16:56:29 +0100

    Update db model and scripts

--- database/inserts/driverAvailability.sql
+++
@@ -1,6 +0,0 @@
-/*
--- Query: 
--- Date: 2018-02-04 16:00
-*/
-INSERT INTO `DriverAvailability` (`id`,`start`,`end`,`days`,`driverID`) VALUES (1,'07:00:00','04:00:00','Mon,Tue,Wed,Thu,Fri',3);
-INSERT INTO `DriverAvailability` (`id`,`start`,`end`,`days`,`driverID`) VALUES (2,'07:00:00','04:00:00','Sat,Sun',5);
