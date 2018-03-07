5ced334e4d9ee6ee8be9884cbfe4e9cc49d073ab
Lucano Zganec <lxz7884@g.rit.edu>
2018-03-07 22:34:34 +0100

    Add DB setup for tests and 1 basic API test

---
+++ server/tests/UsersTest.php
@@ -0,0 +1,27 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class UsersTest extends TestCase
+{
+   private $http;
+
+   public function setUp()
+   {
+      $this->http = new GuzzleHttp\Client(['base_uri' => 'localhost:8000/api/users.php']);
+   }
+
+   public function tearDown()
+   {
+      $this->http = null;
+   }
+
+   public function testGet()
+   {
+      $response = $this->http->request('GET', 'users.php');
+
+      $this->assertEquals(200, $response->getStatusCode());
+
+      $contentType = $response->getHeaders()["Content-Type"][0];
+      $this->assertEquals("application/json", $contentType);
+   }
+}
